709e45a8793a86fee95a2fb303fa686bd8b38719
nurmadinah029 <147220953+nurmadinah029@users.noreply.github.com>
2023-10-20 20:48:30 +0800

    day12.java

---
+++ day12.java
@@ -0,0 +1,20 @@
+package seratushari;
+public class day12 {
+    public static void main(String[] args) {
+        System.out.println("HELLOW WORD ");  
+        double angka1,angka2;
+        angka1 = 25;
+        angka2 = 45;
+        //saya menggunakan metode penugasan
+        
+       angka1 += angka2; // ini sama saja dengan angka1 + angka2 
+       System.out.println(angka1);
+       angka1 -= angka2; // ini sama saja dengan angka1 - angka2 
+       System.out.println(angka1);
+       angka1 *= angka2; // ini sama saja dengan angka1 * angka2 
+       System.out.println(angka1);
+       angka1 /= angka2; // ini sama saja dengan angka1 / angka2 
+       System.out.println(angka1);
+    }
+    
+}
